61156dca6fde03556155c96c8ebeb38d754c46db
kiril <ksfilade@gmail.com>
2021-08-25 15:34:07 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config();
+
+// app configuration
+app.set('port', (process.env.PORT || 3000));
+
+console.log(process.env.PORT)
+console.log('object')
+// setup our express application
+app.use(morgan('dev')); // log every request to the console.
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json()); 
+
+
+// app routes
+require('./routes/webhook_verify')(app);
+
+// warming up the engines !! setta !! go !!!.
+app.listen(app.get('port'), function() {
+  const url = 'http://localhost:' + app.set('port');
+  console.log('Application running on port: ', app.get('port'));
+});
